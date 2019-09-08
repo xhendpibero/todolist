3ec2c450c9977994b8a39c55dfdd085ce8ab262e
Dendy Sapto A <44661300+xhendpibero@users.noreply.github.com>
2019-09-08 15:48:26 +0700

    Progress
    loading

--- src/sagas/saga.js
+++ src/sagas/saga.js
@@ -1,38 +1,42 @@
 import { takeLatest, takeEvery, put, delay } from 'redux-saga/effects';
 
+const progress = (value) =>{
+  return put({ type: 'LOADING_LIST', value });
+}
+
 function* addTodo() {
-  yield put({type: 'LOADING_LIST', value:true});
+  yield progress(true);
   yield delay(500);
   yield put({type: 'ADD_LIST', value:false});
-  yield put({type: 'LOADING_LIST', value:false});
+  yield progress(false);
 }
 
 function* editTodo(action) {
   yield put({type: 'EDIT_LIST', value:action.value});
-  yield put({type: 'LOADING_LIST', value:true});
+  yield progress(true);
   yield delay(100);
-  yield put({type: 'LOADING_LIST', value:false});
+  yield progress(false);
 }
 
 function* deleteTodo(action) {
-  yield put({type: 'LOADING_LIST', value:true});
+  yield progress(true);
   yield delay(500);
   yield put({type: 'DELETE_LIST', value:action.value});
-  yield put({type: 'LOADING_LIST', value:false});
+  yield progress(false);
 }
 
 function* todoDone(action) {
-  yield put({type: 'LOADING_LIST', value:true});
+  yield progress(true);
   yield delay(500);
   yield put({type: 'LIST_DONE', value:action.value});
-  yield put({type: 'LOADING_LIST', value:false});
+  yield progress(false);
 }
 
 function* changeFilter(action) {
-  yield put({type: 'LOADING_LIST', value:true});
+  yield progress(true);
   yield delay(500);
   yield put({type: 'CHANGE_FILTER', value:action.value});
-  yield put({type: 'LOADING_LIST', value:false});
+  yield progress(false);
 }
 
 export function* sagaAddTodo(){
